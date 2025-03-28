b9cf8b6a1bb025ea0750dd0a50adca4089514fab
iamdramaticleo <iamdramaticleo@gmail.com>
2025-03-28 19:15:58 +0200

    Add basic window creation using GLFW
    Integrated GLFW to create a window for the application. Updated CMakeLists.txt to link the GLFW library and set up the necessary sources.

--- code/main.cpp
+++ code/main.cpp
@@ -1,4 +1,21 @@
+#include <GLFW/glfw3.h>
+
 auto main() -> int
 {
+    glfwInit();
+
+    const auto window = glfwCreateWindow(1000, 800, "Tic-Tac-Toe", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
