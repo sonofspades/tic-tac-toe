aa30c7cc8f19a0b6644da4eb6e499d465d05ac51
iamdramaticleo <iamdramaticleo@gmail.com>
2025-03-28 19:51:04 +0200

    Add basic OpenGL buffer setup and draw call
    Introduced vertex and element buffer objects (VBO and EBO) to store vertex and index data. Configured a vertex attribute pointer and issued a `glDrawElements` call to render points. This establishes a foundation for rendering with OpenGL.

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <vector>
+
 auto main() -> int
 {
     glfwInit();
@@ -11,6 +13,32 @@ auto main() -> int
 
     gladLoadGL();
 
+    const std::vector vertices
+    {
+        0.0f, 0.0f, 0.0f
+    };
+
+    const std::vector<uint32_t> elements
+    {
+        0
+    };
+
+    uint32_t vao, vbo, ebo;
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * elements.size(), elements.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 
     while (!glfwWindowShouldClose(window))
@@ -18,6 +46,8 @@ auto main() -> int
         glfwPollEvents();
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawElements(GL_POINTS, elements.size(), GL_UNSIGNED_INT, nullptr);
 
         glfwSwapBuffers(window);
     }
